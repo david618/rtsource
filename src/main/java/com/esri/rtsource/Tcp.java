19bd0c8b082b59bbe2c1612246f44a1af1815454
david <djennings@esri.com>
2016-06-28 09:43:37 -0500

    Couple of small changes

--- src/main/java/com/esri/rtsource/Tcp.java
+++ src/main/java/com/esri/rtsource/Tcp.java
@@ -7,7 +7,6 @@ package com.esri.rtsource;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.time.Duration;
